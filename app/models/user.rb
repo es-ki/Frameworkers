a7520ef4e5ad03166bdd2e39ebcd85e4335b6471
es-ki <aa31568@gmail.com>
2024-12-29 21:39:24 +0900

    モデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  validates :private_id, presence: true, uniqueness: true
+  validates :email, presence: true, uniqueness: true
+
+  has_many :projects, foreign_key: 'user_private_id', primary_key: 'private_id'
 end
