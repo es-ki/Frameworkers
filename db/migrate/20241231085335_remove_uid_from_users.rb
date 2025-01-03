553cdd2f2fdcbcb4f07bff592d476df39d02a3e1
Botofune <48976713+es-ki@users.noreply.github.com>
2025-01-03 10:31:53 +0900

    Revert "CI/CD設定"

--- db/migrate/20241231085335_remove_uid_from_users.rb
+++ db/migrate/20241231085335_remove_uid_from_users.rb
@@ -1,7 +1,5 @@
 class RemoveUidFromUsers < ActiveRecord::Migration[8.0]
   def change
-    if column_exists?(:users, :uid)
-      remove_column :users, :uid, :string
-    end
+    remove_column :users, :uid, :string
   end
 end
