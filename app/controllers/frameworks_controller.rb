bd2cffecc1ada01b66fe3be525126c741a6fc0c8
es-ki <aa31568@gmail.com>
2025-01-04 13:50:31 +0900

    brakeman対応

--- app/controllers/frameworks_controller.rb
+++ app/controllers/frameworks_controller.rb
@@ -9,17 +9,28 @@ class FrameworksController < ApplicationController
       {
         public_id: project[:public_id],
         name: project[:name]
-     }
+      }
     end.compact
 
     if params[:pid].present?
       project = current_user.projects.find_by(public_id: params[:pid])
-      @analysis = project.send("#{params[:id]}s").order(created_at: :desc).first if project
+      @analysis = get_analysis(project, params[:id]) if project
     else
       project = current_user.projects.first
-      @analysis = project.send("#{params[:id]}s").order(created_at: :desc).first if project
+      @analysis = get_analysis(project, params[:id]) if project
     end
   rescue ActiveRecord::RecordNotFound
     redirect_to root_path
+  end
+
+  private
+
+  def get_analysis(project, framework_id)
+    case framework_id
+    when "swot"
+      project.swots.order(created_at: :desc).first
+    else
+      nil
+    end
   end
 end
